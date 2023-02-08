b0abb3179a23fef12c663b6c53a7e1200d394168
narawit6513113 <narawit.pha@student.mahidol.edu>
2023-02-08 17:18:44 +0700

    linked lsit

--- main.c
+++ main.c
@@ -11,14 +11,64 @@
 
 int main(int argc, const char * argv[]) {
     int c=5;
-    struct node a,b,*head ;
+    struct node *head ;
+
+    typedef struct node Node;
+    typedef Node* NodePtr;
+
+    NodePtr p = (NodePtr) malloc (sizeof(Node));
+    head = p;
+    p-> value = c;
+    int i,n = 4;
+    for ( i = 0 ;i < n ; i++)
+    {
+        p->next = (NodePtr) malloc (sizeof(Node));
+        p = p->next;
+        p->value = c+i+1;
+        p->next = NULL;
+
+    }
+
+    typedef struct node* NodePtr;
+    NodePtr tmp=head;
+    while(tmp != NULL)
+    {
+        printf("%3d" , tmp->value);
+        tmp = tmp->next;
+    }printf("   NULL\n");
+    tmp = head;
+    while(tmp != NULL)
+    {
+        struct node* newtmp = tmp->next;
+        printf("deleting %d\n",tmp -> value);
+        free(tmp);
+        tmp = newtmp;
+    }
+    /*p-> next = (NodePtr) malloc (sizeof(Node));
+    p-> next->value = head -> value +3;
+
+    p->next->next = (NodePtr) malloc (sizeof(Node));
+    p->next->next->value = 11;
+    p->next->next->next = NULL;*/
+    
+    /*p-> value = c;
+    p-> next = &b;
+    head = p;
+    head=&z;
+    z.next=&a;
     a.value = c;
     a.next=&b;
-    head=&a;
-    b.value=head->value+3;
-
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
+    z.value = 2;
+    b.value=head->next->value+3;
+    b.next=&d;
+    d.value=11;*/
+    /*printf("%d\n", head ->value ); //what value for 5
+    printf("%d\n", head ->next->value );
+    printf("%d\n", head ->next->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->next->value);
+    printf("%d\n", head ->next->next->next->next->value);
+    // printf("%d\n", head ->next->next->next->next->next->value);
+    printf("NULL\n");*/
 /*  Exercise I
     1. Add 1 more than at the end
     2. Add value(11)
@@ -30,8 +80,20 @@ int main(int argc, const char * argv[]) {
         2. Add value (2)
         
 */
-    typedef struct node* NodePtr;
+    /*typedef struct node* NodePtr;
     NodePtr tmp=head; //add temp value to faciliate
+    int i,n = 4;
+    /*for (i = 0 ; i < n ; i++)
+    {
+        printf("%3d ->",tmp->value);
+        tmp=tmp->next;
+    }*/
+    /*while(tmp != NULL)
+    {
+        printf("%3d" , tmp->value);
+        tmp = tmp->next;
+    }*/
+    
         
     /*  Exercise III Use loop to print everything
         int i,n=5;
